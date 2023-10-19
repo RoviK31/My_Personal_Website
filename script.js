af4273ad2e1d0c1686c74b6413647c2068957e7c
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-10-19 15:49:11 +0800

    add smooth scrolling to top

--- script.js
+++ script.js
@@ -10,5 +10,46 @@ const bodyEl = document.querySelector('body')
 
 btnNav.addEventListener('click', function(){
   bodyEl.classList.toggle('nav-open')
-  
 })
+
+//smooth scrolling
+const allLinks = document.querySelectorAll('a:link');
+console.log(allLinks);
+
+allLinks.forEach(link =>{
+  link.addEventListener('click', function(e){
+    e.preventDefault()
+    const href = link.getAttribute('href')
+
+    // scroll back to top
+    if(href === "#"){
+      window.scrollTo({
+        top:0,
+        behavior: "smooth"
+      })
+    }
+
+ 
+  })
+})
+
+
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
