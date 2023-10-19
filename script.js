a0954c9bccca1f3ea0f3798cc53b25723bdb6740
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-10-19 17:59:08 +0800

    add lazy loading img & small changes

--- script.js
+++ script.js
@@ -105,10 +105,40 @@ const techObserver = new IntersectionObserver(function(entries){
 
  })
 
+},{
+  root:null,
+  threshold:0,
 })
 tech_section.forEach(icon=>{
   techObserver.observe(icon)
 })
+
+// lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]')
+// console.log(imgTargets);
+
+const loadImg = function(entries, observer){
+  const [entry] = entries
+  console.log(entry);
+
+  if(!entry.isIntersecting) return
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img')
+  })
+
+  observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadImg,{
+  root:null,
+  threshold: 0,
+  rootMargin: '100px'
+})
+
+imgTargets.forEach(img=> imgObserver.observe(img))
 
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
